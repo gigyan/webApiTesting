17c152d30c1d7ae3fce6dd77298d10e51ed51ae1
oProximity <39976780+oProximity@users.noreply.github.com>
2021-11-15 19:12:09 -0500

    Add project files.

---
+++ server.js
@@ -0,0 +1,41 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+var token;
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId: '4c6ad1d4aec64c6788d2a80f1c3bfb77',
+    clientSecret: 'b2cecac97b384ea9aa9b00cda8e90037',
+    //redirectUri: 'http://www.example.com/callback'
+});
+
+spotifyApi.clientCredentialsGrant().
+    then(function (result) {
+        spotifyApi.setAccessToken(result.body['access_token']);
+
+        var userInput = 'slice the cake';
+        var artistURI;
+
+         spotifyApi.searchArtists(userInput).then(function (data) {
+            console.log(data.body);
+            artistURI = data.body.artists.items[0].id;
+            console.log(artistURI);
+
+        }, function(err) {
+            console.error(err);
+        });
+
+        return spotifyApi.getArtistAlbums(artistURI,'US').then(
+            function (data) {
+                console.log('Artist albums', data.body);
+                
+            },
+            function (err) {
+                console.error(err);
+            }
+        );
+
+    }).catch(function (err) {
+        console.log('somethings fucked man');
+        console.log('Hint: ');
+        console.log(err);
+    });
